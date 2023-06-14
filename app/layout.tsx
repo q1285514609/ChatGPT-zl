c7b459072470d5e4023d121be93c5a1776c10914
zhouliang <1285514609@qq.com>
2023-06-14 11:21:15 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getBuildConfig } from "./config/build";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "免魔法ChatGPT Web",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "免魔法ChatGPT Web",
     statusBarStyle: "default",
   },
 };
